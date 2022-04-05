dd1e8a8b815880648446d910be3ddc5200440a67
zyy <18733505010@163.com>
2022-04-05 13:24:53 +0800

    feat(): 初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
 module.exports = {
   extends: [require.resolve('@healthdt/linter/dist/eslint')],
   globals: {
-    page: true,
     REACT_APP_ENV: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': [
+      2,
+      {
+        ignore: ['@healthdt/application-detail'], // 请替换成你的包名 application-detail
+        caseSensitive: true,
+        commonjs: true,
+      },
+    ],
   },
 };
